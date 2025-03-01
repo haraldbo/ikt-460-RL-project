58f2a3c7809db9e922163fa874bb8b6a8f8ad7be
Harald Borgenvik <8281021+haraldbo@users.noreply.github.com>
2025-03-01 16:36:06 +0100

    Update example.py
    update key event handler

--- example.py
+++ example.py
@@ -6,29 +6,39 @@ import pygame
 class HumanPlayerAgent(Agent):
 
     def __init__(self):
-        self.next_action = None
+        self.next_action = (0, 0)
 
     def get_action(self, environment: Environment):
-        if self.next_action != None:
-            action = self.next_action
-            self.next_action = None
-            return action
+        action = (
+            max(-1, min(self.next_action[0], 1)),
+            max(-1, min(self.next_action[1], 1))
+        )
 
-        return (0, 0)
+        self.next_action = (0, 0)
+        return action
 
     def render(self, window: pygame.Surface):
         pass
 
     def handle_event(self, event: pygame.event.Event):
         if event.type == pygame.KEYDOWN:
+            gimbal_action = 0
+            thrust_action = 0
+
             if event.key == pygame.K_UP:
-                self.next_action = (0, 1)
-            if event.key == pygame.K_LEFT:
-                self.next_action = (1, 0)
-            if event.key == pygame.K_RIGHT:
-                self.next_action = (-1, 0)
+                thrust_action = 1
             if event.key == pygame.K_DOWN:
-                self.next_action = (0, -1)
+                thrust_action = -1
+            if event.key == pygame.K_LEFT:
+                gimbal_action = 1
+            if event.key == pygame.K_RIGHT:
+                gimbal_action = -1
+
+            # Done to allow actions such as (1, -1):
+            self.next_action = (
+                self.next_action[0] + gimbal_action,
+                self.next_action[1] + thrust_action
+            )
 
     def reset(self):
         pass
